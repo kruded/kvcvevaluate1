fc69021cd3e11a3edebf9af5d6d4511b4068b636
kruded <30819672+kruded@users.noreply.github.com>
2017-10-23 09:29:22 +0700

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -66180,11 +66180,11 @@ angular.module('mm.addons.mod_wiki')
 angular.module('mm.core')
 
 .constant('mmCoreConfigConstants', {
-    "app_id" : "kvc.moodle.moodlemobile",
-    "appname": "KVC Moodle Mobile",
+    "app_id" : "eva.kvc.moodle.moodlemobile",
+    "appname": "EVA KVC Moodle Mobile",
     "desktopappname": "Moodle Desktop",
-    "versioncode" : "20171022",
-    "versionname" : "3.3.2",
+    "versioncode" : "20171023",
+    "versionname" : "1.0.0",
     "cache_expiration_time" : 300000,
     "default_lang" : "en",
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch", "de-du": "Deutsch - Du", "el": "Ελληνικά", "en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "lt" : "Lietuvių", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sr-cr": "Српски", "sr-lt": "Srpski", "sv": "Svenska", "tr" : "Türkçe", "uk" : "Українська", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
@@ -66193,7 +66193,7 @@ angular.module('mm.core')
     "demo_sites": {},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "http://202.29.238.158/lms",
+    "siteurl": "http://61.91.109.46/eva2",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
     "privacypolicy": "https://moodle.org/mod/page/view.php?id=8148"
